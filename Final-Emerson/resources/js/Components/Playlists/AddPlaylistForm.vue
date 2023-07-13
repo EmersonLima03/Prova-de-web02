51b77081596ec8fdf6132cb543ae1e35d5989ebd
Aluno Lab 02 <aluno.lab02@ifpe.edu>
2023-07-13 12:38:13 -0300

    primeiro comiit

---
+++ Final-Emerson/resources/js/Components/Playlists/AddPlaylistForm.vue
@@ -0,0 +1,34 @@
+<script setup>
+import MyTextInput from '../MyTextInput.vue';
+import { useForm } from '@inertiajs/vue3';
+import PrimaryButton from '../PrimaryButton.vue';
+
+const emit = defineEmits(['playlist-added']);
+
+const form = useForm({
+    title: '',
+});
+
+function submit() {
+    form.post(route('playlists.add'), {
+        onSuccess: () => {
+            form.reset();
+            emit('playlist-added');
+        },
+    });
+}
+</script>
+
+<template>
+    <form>
+        <fieldset class="border shadow-md p-2 rounded-md w-full m-auto">
+            <legend class="font-bold">
+                Add new Playlist
+            </legend>
+            <div>
+                <MyTextInput v-model="form.title" label="Title" autofocus />
+                <PrimaryButton>Save</PrimaryButton>
+            </div>
+        </fieldset>
+    </form>
+</template>
